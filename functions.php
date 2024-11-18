e72c7fb09d25ccf140dc930b7bd04e6e2f0b8ec3
abidomingo <abidomingo25@gmail.com>
2024-11-18 22:24:10 +0800

    Successfully added the database connection

--- functions.php
+++ functions.php
@@ -1,3 +1,14 @@
 <?php    
-    // All project functions should be placed here
+       $host = "localhost";
+       $user = "root";
+       $password = ""; // Default password for Laragon
+       $dbname = "dct_ccs_finals";
+    
+       $conn = new mysqli($host, $user, $password, $dbname);
+    
+    // Check the connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
 ?>
