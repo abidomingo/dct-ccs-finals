f2dc8f59882f0171393e787949a01710af3abe9a
abidomingo <abidomingo25@gmail.com>
2024-11-21 21:37:36 +0800

    Bug successfully fixed

--- admin/partials/side-bar.php
+++ admin/partials/side-bar.php
@@ -7,13 +7,13 @@
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link d-flex align-items-center gap-2" href="/admin/dashboard.php" style="font-weight: <?php echo ($_SERVER['PHP_SELF'] == '/admin/dashboard.php' ? 'bold' : 'normal'); ?>;">
                         <i class="fa-solid fa-gauge fa-fw me-2"></i>
                         Dashboard
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link d-flex align-items-center gap-2" href="/admin/subject/add.php" style="font-weight: <?php echo ($_SERVER['PHP_SELF'] == '/admin/subject/add.php' || $_SERVER['PHP_SELF'] == '/admin/subject/edit.php' || $_SERVER['PHP_SELF'] == '/admin/subject/delete.php' ? 'bold' : 'normal'); ?>;">
                     <i class="fa-solid fa-file fa-fw me-2"></i>
                         Subjects
                     </a>
